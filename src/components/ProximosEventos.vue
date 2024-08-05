fb36ee6459b4f1bb5f04a5ee1765645d69b297ac
duridev <duridev@outlook.com>
2024-08-04 22:49:02 -0400

    first commit

---
+++ src/components/ProximosEventos.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import homeData from '../../src/db/home.json';
+
+const eventos = ref([]);
+
+onMounted(() => {
+  eventos.value = homeData.paginaInicio.eventos;
+});
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center w-2/5 mx-auto">
+    <div v-for="evento in eventos" :key="evento.id" class="bg-white rounded-lg shadow-md p-4 mb-4 relative">
+      <div class="absolute top-2 right-2 text-sm">
+        <div class="flex items-center space-x-1">
+          <span class="text-gray-600">📅</span>
+          <div class="text-right">
+            <p class="text-gray-700">{{ evento.fecha }}</p>
+            <p class="text-gray-700">{{ evento.hora }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <h2 class="text-xl font-bold mb-2">{{ evento.titulo }}</h2>
+        <p class="text-gray-600 mb-2">{{ evento.descripcion }}</p>
+        <p class="text-gray-800 italic">{{ evento.ubicacion }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tematicas.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import tematicasData from '../db/home.json';
+
+const tematicas = ref([]);
+
+onMounted(() => {
+  tematicas.value = tematicasData.paginaInicio.tematicasDestacadas;
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center space-y-4 w-4/5 mx-auto mt-8">
+    <div v-for="tematica in tematicas" :key="tematica.id" class="bg-white rounded-lg shadow-md border border-gray-300 w-full max-w-4xl">
+      <!-- Nombre de la temática -->
+      <div class="bg-gray-200 p-2 border-b border-gray-300">
+        <h2 class="text-xl font-bold text-gray-800">{{ tematica.nombre }}</h2>
+      </div>
+      
+      <!-- Descripción de la temática -->
+      <div class="p-4">
+        <p class="text-gray-600">{{ tematica.descripcion }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Estilos específicos para este componente si es necesario */
+</style>
